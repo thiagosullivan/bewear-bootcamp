9b92ed1e9773a0ed91866e5dba0bdb23a3c71d26
Thiago de Moura Santos <thiagosullivan12@gmail.com>
2025-08-10 22:16:43 -0300

    chore: add eslint, prettier and shadcn setup

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Bem vindo ao Bootcamp</h2>
+    <Button>Enviar!</Button>
+    </div>
+    
+  );
+}
+
+export default Home
