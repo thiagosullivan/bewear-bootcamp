b22d113df73f025e1441bd9a97d59e864be07e25
Thiago de Moura Santos <thiagosullivan12@gmail.com>
2025-08-20 17:11:35 -0300

    feat: add cart component

---
+++ src/components/common/cart.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { ShoppingBasketIcon } from "lucide-react";
+
+import { Button } from "../ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+
+const Cart = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline" size="icon">
+          <ShoppingBasketIcon />
+        </Button>
+      </SheetTrigger>
+      <SheetContent>x</SheetContent>
+    </Sheet>
+  );
+};
+
+export default Cart;
