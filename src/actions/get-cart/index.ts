5a18ddc8c103beda05d56df2fb978533f2f89ea7
Thiago de Moura Santos <thiagosullivan12@gmail.com>
2025-08-21 15:19:26 -0300

    feat: add cart summary

--- src/actions/get-cart/index.ts
+++ src/actions/get-cart/index.ts
@@ -41,5 +41,11 @@ export const getCart = async () => {
       totalPriceInCents: 0,
     };
   }
-  return cart;
+  return {
+    ...cart,
+    totalPriceInCents: cart.items.reduce(
+      (acc, item) => acc + item.productVariant.priceInCents * item.quantity,
+      0
+    ),
+  };
 };
--- src/components/common/cart.tsx
+++ src/components/common/cart.tsx
@@ -5,8 +5,11 @@ import { ShoppingBasketIcon } from "lucide-react";
 import Image from "next/image";
 
 import { getCart } from "@/actions/get-cart";
+import { formatCentsToBRL } from "@/helpers/money";
 
 import { Button } from "../ui/button";
+import { ScrollArea } from "../ui/scroll-area";
+import { Separator } from "../ui/separator";
 import {
   Sheet,
   SheetContent,
@@ -33,19 +36,48 @@ const Cart = () => {
         <SheetHeader>
           <SheetTitle>Carrinho</SheetTitle>
         </SheetHeader>
-        <div className="space-y-4 px-5">
-          {cartIsLoading && <div>Carregando...</div>}
-          {cart?.items.map((item) => (
-            <CartItem
-              key={item.id}
-              id={item.id}
-              productName={item.productVariant.product.name}
-              productVariantName={item.productVariant.name}
-              productVariantImageUrl={item.productVariant.imageUrl}
-              productVariantPriceInCents={item.productVariant.priceInCents}
-              quantity={item.quantity}
-            />
-          ))}
+        <div className="flex h-full flex-col px-5 pb-5">
+          <div className="flex h-full max-h-full flex-col overflow-hidden">
+            <ScrollArea className="h-full">
+              <div className="flex h-full flex-col gap-8">
+                {cart?.items.map((item) => (
+                  <CartItem
+                    key={item.id}
+                    id={item.id}
+                    productName={item.productVariant.product.name}
+                    productVariantName={item.productVariant.name}
+                    productVariantImageUrl={item.productVariant.imageUrl}
+                    productVariantPriceInCents={
+                      item.productVariant.priceInCents
+                    }
+                    quantity={item.quantity}
+                  />
+                ))}
+              </div>
+            </ScrollArea>
+          </div>
+
+          {cart?.items && cart?.items.length > 0 && (
+            <div className="flex flex-col gap-4">
+              <Separator />
+              <div className="flex items-center justify-between text-xs font-medium">
+                <p>Subtotal</p>
+                <p>{formatCentsToBRL(cart?.totalPriceInCents ?? 0)}</p>
+              </div>
+              <Separator />
+              <div className="flex items-center justify-between text-xs font-medium">
+                <p>Entrega</p>
+                <p>Gratis</p>
+              </div>
+              <Separator />
+              <div className="flex items-center justify-between text-xs font-medium">
+                <p>Total</p>
+                <p>{formatCentsToBRL(cart?.totalPriceInCents ?? 0)}</p>
+              </div>
+
+              <Button className="mt-5 rounded-full">Finalizar compra</Button>
+            </div>
+          )}
         </div>
       </SheetContent>
     </Sheet>
